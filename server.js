1a28d3e56162e4c7634ccf46cca5ea82dce16d07
saheduxi <saheduxi@gmail.com>
2025-07-10 06:03:23 +0600

    server.js

--- server.js
+++ server.js
@@ -51,3 +51,19 @@ const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
